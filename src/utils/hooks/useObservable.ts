f1b220bfd4613d848d0341d06f0d5acc74fdcfc8
Andrey Solodov <solodov.dev@gmail.com>
2022-03-19 08:53:58 +0300

    feature/observable: Add rxjs and implement useObservable hook

---
+++ src/utils/hooks/useObservable.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { Observable } from "rxjs";
+
+export default function useObservable<T>(observable: Observable<T>) {
+  const [data, setData] = useState<T>();
+
+  useEffect(() => {
+    const subscription = observable.subscribe(setData);
+    return subscription.unsubscribe();
+  }, [observable]);
+
+  return data;
+}
