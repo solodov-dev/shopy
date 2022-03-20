7e7c81fb3e1fdf3580c79aded93d65435e675651
Andrey Solodov <solodov.dev@gmail.com>
2022-03-20 15:20:16 +0300

    feature/cart: Add catalog product card component

---
+++ src/features/cart/components/productCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CatalogItem } from '../model/schema';
+
+export default function ProductCard(props: {children: CatalogItem}) {
+  return (
+    <div></div>
+  )
+}
