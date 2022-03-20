8d8201811529eb5416def9da59f38ca73de7c07d
Andrey Solodov <solodov.dev@gmail.com>
2022-03-20 09:36:44 +0300

    feature/table: Table styles fix

--- src/ui/components/table.tsx
+++ src/ui/components/table.tsx
@@ -1,6 +1,9 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
 import { ReactNode } from "react";
+import datesAreEqual from "utils/helpers/datesAreEqual";
 import objectKeys from "utils/helpers/objectKeys";
+import isDate from "utils/typeGuards/isDate";
+import isString from "utils/typeGuards/isString";
 import styles from "../styles/table.module.css";
 
 export type ColumnType<T> = {
@@ -20,71 +23,45 @@ export default function DataTable<T>(props: {
 
   return (
     <table className={styles.table}>
-      <DataHeader columns={props.columns} setFilter={setFilter} />
-      <Body columns={props.columns} data={props.data} filter={filter} />
+      <thead>
+        <tr key="tr">
+          {props.columns.map((col) => (
+            <th key={col.title}>
+              <div className={styles.header}>
+                {col.title}
+                {col.form?.(setFilter)}
+              </div>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {props.data
+          .filter((value) => filterTable(value, filter))
+          .map((dataItem, index) => (
+            <tr key={index}>
+              {props.columns.map((col) => (
+                <td key={col.title}>
+                  {col.render ? col.render(dataItem) : dataItem[col.dataIndex]}
+                </td>
+              ))}
+            </tr>
+          ))}
+      </tbody>
     </table>
   );
 }
 
-function filterData<T>(data: T, filter: TableFilter<T>) {
-  const filterKeys = objectKeys(filter);
-  for (const key of filterKeys) {
-    if (typeof data[key] === "string") {
-      if (!new RegExp(filter[key], "i").test(data[key] as unknown as string)) {
-        return;
-      }
-    } else if (filter[key] && data[key] instanceof Date) {
-      if (
-        !(
-          (data[key] as unknown as Date).toDateString() ===
-          (filter[key] as unknown as Date).toDateString()
-        )
-      ) {
-        return;
-      }
+
+function filterTable<T>(rowData: T, tableFilter: TableFilter<T>) {
+  for (const key of objectKeys(tableFilter)) {
+    const filter = tableFilter[key];
+    const value = rowData[key];
+    if (filter) {
+      if (isString(value) && !new RegExp(filter, "i").test(value)) return; 
+      if (isDate(value) && isDate(filter) && !datesAreEqual(value, filter)) return;
     }
   }
-  return data;
+  return rowData;
 }
 
-function Body<T>(props: {
-  data: T[];
-  columns: ColumnType<T>[];
-  filter: TableFilter<T>;
-}) {
-  return (
-    <tbody>
-      {props.data
-        .filter((value) => filterData(value, props.filter))
-        .map((dataItem, index) => (
-          <tr key={index}>
-            {props.columns.map((col) => (
-              <td key={col.title}>
-                {col.render ? col.render(dataItem) : dataItem[col.dataIndex]}
-              </td>
-            ))}
-          </tr>
-        ))}
-    </tbody>
-  );
-}
-
-function DataHeader<T>(props: {
-  columns: ColumnType<T>[];
-  setFilter: Dispatch<SetStateAction<TableFilter<T>>>;
-}) {
-  return (
-    <thead>
-      <tr key="tr">
-        {props.columns.map((col) => (
-          <th key={col.title}>
-            <div className={styles.header}>
-              {col.title}
-              {col.form?.(props.setFilter)}
-            </div>
-          </th>
-        ))}
-      </tr>
-    </thead>
-  );
-}
